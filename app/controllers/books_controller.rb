f04ac681261a34ab3ab36bdf55f95be1faf46ef0
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-20 09:41:25 +0000

    [Add]:更新機能、一覧画面追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+  
+  def create 
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
+  def edit
+  end
+
+  def show
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
+  # get 'books/index'
+  # get 'books/new'
+  # get 'books/edit'
+  # get 'books/show'
+  devise_for :users
+  # get 'homes/top'
+  root to: "homes#top"
+  get 'homes/about', as: 'about'
+  resources :books
+  resources :users, only: [:new, :index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
